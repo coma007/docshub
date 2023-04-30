f676d0ee073104d2b6d66cec5a9f6f2224393ea9
coma007 <milicasladakovic7@gmail.com>
2023-04-30 15:38:23 +0200

    Connected Dynamo db to backend

--- docshub-back/app.py
+++ docshub-back/app.py
@@ -1,13 +1,18 @@
 import boto3
+import aws_config
 from flask import Flask, request
 
 app = Flask(__name__)
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('FileMetadata')
+dynamodb = boto3.resource('dynamodb',
+			aws_access_key_id=aws_config.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=aws_config.AWS_SECRET_ACCESS_KEY,
+            region_name=aws_config.AWS_REGION)
+table = dynamodb.Table('file_metadata')
 
 @app.route('/api/upload-file', methods=['POST'])
 def upload_file():
+    album_id = request.json['albumId']
     file_id = request.json['fileId']
     file_size = request.json['size']
     file_type = request.json['type']
@@ -16,16 +21,16 @@ def upload_file():
     creation_date = request.json['date']
     tags = request.json['tags']
 
-    # Store the file metadata in DynamoDB
     table.put_item(Item={
-        'file_id': file_id,
-        'file_size' : file_size,
-        'file_type' : file_type,
-	'name' : name,
-        'description': description,
-        'creation_date': creation_date,
-        'last_change_date': creation_date,
-        'tags': tags,
+        'album_id': '1',
+        'file_id': 'file_id',
+        'file_size' : 'file_size',
+        'file_type' : 'file_type',
+	    'name' : 'name',
+        'description': 'description',
+        'creation_date': 'creation_date',
+        'last_change_date': 'creation_date',
+        'tags': 'tags',
     })
 
     return ''
