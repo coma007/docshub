0a25d983b08dc6692a7821d73a8e631b29f5f7f4
coma007 <milicasladakovic7@gmail.com>
2023-05-02 18:52:08 +0200

    Configured aws and cors

--- docshub-back/app.py
+++ docshub-back/app.py
@@ -1,8 +1,10 @@
 import boto3
 import aws_config
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000'])
 
 dynamodb = boto3.resource('dynamodb',
 			aws_access_key_id=aws_config.AWS_ACCESS_KEY_ID,
@@ -13,24 +15,23 @@ table = dynamodb.Table('file_metadata')
 @app.route('/api/upload-file', methods=['POST'])
 def upload_file():
     album_id = request.json['albumId']
-    file_id = request.json['fileId']
-    file_size = request.json['size']
-    file_type = request.json['type']
-    name = request.json['name']
+    file_id = request.json['fileName']
+    file_size = request.json['fileSize']
+    file_type = request.json['fileType']
     description = request.json['description']
-    creation_date = request.json['date']
+    creation_date = request.json['dateOfCreation']
     tags = request.json['tags']
 
     table.put_item(Item={
-        'album_id': '1',
-        'file_id': 'file_id',
-        'file_size' : 'file_size',
-        'file_type' : 'file_type',
-	    'name' : 'name',
-        'description': 'description',
-        'creation_date': 'creation_date',
-        'last_change_date': 'creation_date',
-        'tags': 'tags',
+        'album_id': album_id,
+        'file_id': file_id,
+        'file_size' : file_size,
+        'file_type' : file_type,
+	    'name' : file_id,
+        'description': description,
+        'creation_date': creation_date,
+        'last_change_date': creation_date,
+        'tags': tags,
     })
 
     return ''
