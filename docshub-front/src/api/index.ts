a1d50c337ded99fc7175d71f41b8a78bbc6e925a
coma007 <milicasladakovic7@gmail.com>
2023-06-19 13:29:46 +0200

    Changed app url

--- docshub-front/src/api/index.ts
+++ docshub-front/src/api/index.ts
@@ -1,3 +1,4 @@
-const url = "http://localhost:5000/api/"
+// const url = "http://localhost:5000/api/"
+const url = "https://95dxj4ei9c.execute-api.eu-central-1.amazonaws.com/test/api/"
 
 export const upload_url = url + "upload-file";
