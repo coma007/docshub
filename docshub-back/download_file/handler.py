696a22688ae51fb96654c5ee5f8638f065ae6b2a
coma007 <milicasladakovic7@gmail.com>
2023-06-23 22:30:00 +0200

    Added file download lambda

---
+++ docshub-back/download_file/handler.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+import base64
+
+from utils.s3_config import s3, s3_bucket_name
+from utils.dynamodb_config import table
+
+from utils.response import create_response
+
+
+def download_file(event, context):
+    try:
+        file_key = event['fileKey']
+        response = s3.get_object(Bucket=s3_bucket_name, Key="public/" + file_key)
+
+        
+        file_data = base64.b64encode(response.get("Body").read()).decode("utf-8")
+
+        return {
+            'statusCode': 200,
+            'body': file_data,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': response['ContentType'],
+                'Content-Disposition': f'attachment; filename={file_key}'
+            }
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(e, default=str)
+        }
--- docshub-back/utils/s3_config.py
+++ docshub-back/utils/s3_config.py
@@ -5,4 +5,4 @@ s3 = boto3.client('s3',
                   aws_access_key_id=aws_config.AWS_ACCESS_KEY_ID,
                   aws_secret_access_key=aws_config.AWS_SECRET_ACCESS_KEY,
                   region_name=aws_config.AWS_REGION)
-s3_bucket_name = "docshub-files"
+s3_bucket_name = "docshub-storage104417-staging"
