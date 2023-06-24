d18cbb8b8b9e95abb7de8a9fe58866d166942072
coma007 <milicasladakovic7@gmail.com>
2023-06-24 18:57:01 +0200

    Lambda for file metadata

---
+++ docshub-back/get_file_metadata/handler.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+
+from utils.s3_config import s3, s3_bucket_name
+from utils.dynamodb_config import table, table_name
+
+from utils.response import create_response
+
+
+def get_file_metadata(event, context):
+    fileKey = event['pathParameters']['key']
+    try:
+        response = table.get_item(
+            TableName=table_name,
+            Key={
+                'album_id': 'ALBUM',
+                'file': fileKey
+            }
+        )
+        return create_response(200, response['Item'])
+
+    except Exception as e:
+        return create_response(500, e)
