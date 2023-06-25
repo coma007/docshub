62088896962fd83ce97f808894070b33b459d1a3
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2023-06-25 09:56:42 +0200

    Added file validation

--- docshub-front/src/features/file-upload/services/FileUploadService.tsx
+++ docshub-front/src/features/file-upload/services/FileUploadService.tsx
@@ -2,10 +2,31 @@ import axios from 'axios';
 import { UPLOAD_FILE_FILE_URL, UPLOAD_FILE_URL } from '../../../api';
 import { FileMetadata, FileMetadataWithFile } from '../../../types/FileMetadata';
 
+const getFileSize = (base64: string) => {
+    let stringLength = base64.length;
+    let sizeInBytes = 4 * Math.ceil((stringLength / 3))*0.5624896334383812;
+    let sizeInKb=sizeInBytes/1000;
+    let sizeInMb=sizeInKb/1000;
+    return sizeInMb;
+}
+
+const validateFile = (data: FileMetadataWithFile) => {
+    if(getFileSize(data.file) > 10 || (data.fileSize/1000)/1000 > 6){
+        return false;
+    }
+
+    let isRightType = ["image/", "application/", "text/", 'video/', "audio"].some(substr => data.fileType?.startsWith(substr));
+    if(!isRightType)
+        return false;
+    return true;
+}
+
 const FileUploadService = {
 
     upload: async function (data: FileMetadataWithFile): Promise<boolean> {
         try {
+            if(!validateFile(data))
+                return false;
             const response = await axios.post(UPLOAD_FILE_FILE_URL(), 
             {
                 input: JSON.stringify({
