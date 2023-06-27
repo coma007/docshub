47525ab9c98b8a5a9a0ae1c7080ba08200ef04d7
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2023-06-27 20:18:30 +0200

    feat: create album s3

--- docshub-back/create_album/handler.py
+++ docshub-back/create_album/handler.py
@@ -14,6 +14,8 @@ def create_album(event, context):
         album_id = body['albumId']
         album_name = body['albumName']
     
+        s3.put_object(Bucket=s3_bucket_name, Key=("public/" + parent_album_id + "/" + album_id + "/"))
+    
         album_table.put_item(Item={
             'album_id': album_id,
             'parent_album_id': parent_album_id,
