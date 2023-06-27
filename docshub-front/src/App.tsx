e3086e29bfcbd64983ee784b751ec198b02cfa5f
coma007 <milicasladakovic7@gmail.com>
2023-06-27 22:08:56 +0200

    Referal as field in sign up

--- docshub-front/src/App.tsx
+++ docshub-front/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { withAuthenticator, useAuthenticator, Button } from "@aws-amplify/ui-react"
+import { withAuthenticator, useAuthenticator, Button, Authenticator } from "@aws-amplify/ui-react"
 import "@aws-amplify/ui-react/styles.css"
 import FileListPage from './features/file-list/pages/FileListPage';
 import Navigation from './components/Navigation/Navigation';
@@ -8,12 +8,25 @@ import { Auth } from 'aws-amplify';
 
 function App() {
 
+  const signUpFields = {
+    signUp: {
+      'custom:referal_email': {
+        placeHolder: "Enter Referal Email",
+        isRequired: false,
+        label: 'Referal Email',
+        order: 10
+      },
+    }
+  }
+
   return (
-    <div className="App">
-      <Navigation />
-      <FileListPage />
-    </div >
+    <Authenticator formFields={signUpFields} >
+      <div className="App">
+        <Navigation />
+        <FileListPage />
+      </div >
+    </Authenticator>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
