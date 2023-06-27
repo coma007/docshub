23240c560435c69428f5db0a0ed2656270b65e68
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2023-06-27 20:20:33 +0200

    Patched create album

--- docshub-back/create_album/handler.py
+++ docshub-back/create_album/handler.py
@@ -13,6 +13,7 @@ def create_album(event, context):
         parent_album_id = body['parentAlbumId']
         album_id = body['albumId']
         album_name = body['albumName']
+        s3.put_object(Bucket=s3_bucket_name, Key=("public/" + parent_album_id + "/" + album_id + "/"))
     
         album_table.put_item(Item={
             'album_id': album_id,
