1100e76adde33b4625348fe0c465eaf8180b4b22
coma007 <milicasladakovic7@gmail.com>
2023-06-28 11:22:54 +0200

    User access fix

--- docshub-back/get_users_with_access/handler.py
+++ docshub-back/get_users_with_access/handler.py
@@ -7,26 +7,25 @@ from utils.response import create_response
 
 def get_users_with_access(event, context):
     item_path = event['queryStringParameters']['fileName']
+    print(item_path)
 
     table_name = 'permissions'
     table = dynamodb.Table(table_name)
 
-    try:
-        response = table.query(
-            IndexName="item_path-index",
-            KeyConditionExpression=Key('item_path').eq(item_path),
-        )
-        print(response)
-
-        usernames = [item['username'] for item in response['Items']]
-
-        return {
-            'statusCode': 200,
-            'body': usernames
+    params = {
+        'TableName': table_name,
+        'IndexName': 'file_name-index',
+        'KeyConditionExpression': 'file_name = :item_path',
+        'ExpressionAttributeValues': {
+            # Assuming file_name is of type 'S' (String)
+            ':item_path': item_path
         }
+    }
+
+    try:
+        response = table.query(**params)
+
+        return create_response(200, response['Items'])
 
     except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
+        return create_response(500, e)
