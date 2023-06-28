e78c099aea254bfeb2025fe7aea2f31bafaeba70
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2023-06-28 17:05:46 +0000

    Missing lambda

--- docshub-back/add_user_access/handler.py
+++ docshub-back/add_user_access/handler.py
@@ -7,15 +7,15 @@ from utils.response import create_response
 permissions_table_name = 'permissions'
 permissions_table = dynamodb.Table(permissions_table_name)
 
-
 def add_user_access(event, context):
 
     users = event['users']
     file_name = event['fileKey']
 
+
     try:
         results = []
-
+        
         if file_name.endswith("/"):
             for user in users:
                 results.append(create_album_permission(user, file_name))
@@ -24,49 +24,44 @@ def add_user_access(event, context):
                 results.append(create_file_permission(user, file_name))
 
         return create_response(200, results)
-
+        
     except Exception as e:
         return create_response(500, e)
 
 
 def create_album_permission(username, file_name):
-    albums = {}
+    albums = set()
     items = table.scan()['Items']
     for item in items:
-        if item['album_id'].startswith(file_name):
+        if item['album_id'].startswith(file_name[:-1]):
             albums.add(item['album_id'])
-
     files = []
-    for album in albums:
-        files.append(table.query(
-            TableName=table_name,
-            KeyConditionExpression='album_id = :val',
-            ExpressionAttributeValues={
-                ':val': album
-            }
-        )["Items"])
-
     results = []
+    for album in albums:
+        files += table.query(
+                KeyConditionExpression=Key('album_id').eq(album)
+        )["Items"]
+        results.append(create_file_permission(username, album + "/"))
+        
     for file in files:
-        results.append(create_file_permission(username, file))
+        results.append(create_file_permission(username, file["album_id"] + "/" + file["file_name"]))
     return results
 
 
 def create_file_permission(username, file_name):
     if access_exists(username, file_name):
         return permissions_table.get_item(
-            Key={
-                'username': username,
-                'file_name': file_name
-            }
-        )
+                Key={
+                    'username': username,
+                    'file_name': file_name
+                }
+            )
     result = permissions_table.put_item(Item={
-        'username': username,
-        'file_name': file_name
+        'username' : username,
+        'file_name' : file_name
     })
-
+    
     return result
-
 
 def access_exists(username, file_name):
     response = permissions_table.get_item(
